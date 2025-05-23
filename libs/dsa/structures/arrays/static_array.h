7b4d9861d0b5acbb587f87e4387a5830d319eb17
AdrianRo147 <adrian.rovnan@gmail.com>
2025-05-23 18:37:38 +0200

    documentation comments

--- libs/dsa/structures/arrays/static_array.h
+++ libs/dsa/structures/arrays/static_array.h
@@ -7,18 +7,36 @@
 
 namespace dsa::structures
 {
+    /**
+      * @brief Simple static array implementation
+      *
+      * Provides basic static array functionality such as setting at index, getting element at index,
+      * initializer_list constructor and assignment operator.
+      * Elements are stored in heap allocated array
+      *
+      * @tparam T Type of Elements
+      * @tparam size Size of array
+      */
     template<typename T, size_t size>
     class StaticArray
     {
     private:
-        T* pElements;
+        T* pElements; /// Pointer to elements array.
 
     public:
+        /**
+         * @brief Default constructor. Initializes an empty array.
+         */
         StaticArray()
         {
             this->pElements = new T[size]();
         }
 
+        /**
+         * @brief Constructs the array from an initializer list.
+         * @param list Initializer list of array elements.
+         * @throws std::runtime_error if list size does not match array size
+         */
         StaticArray(const std::initializer_list<T> list)
         {
             if (size != list.size())
@@ -37,7 +55,10 @@ namespace dsa::structures
             }
         }
 
-        // copy constructor
+        /**
+         * @brief Copy constructor. Creates deep copy of another StaticArray.
+         * @param other StaticArray to copy from.
+         */
         StaticArray(const StaticArray<T, size>& other)
         {
             this->pElements = new T[size]();
@@ -47,19 +68,34 @@ namespace dsa::structures
             }
         }
 
-        // move constructor
+        /**
+         * @brief Move constructor. Transfers ownership of resources form another StaticArray.
+         *
+         * Moves the pElements array from one instance to another.
+         *
+         * @param other StaticArray to move from.
+         */
         StaticArray(StaticArray<T, size>&& other) noexcept
         {
             this->pElements = other.pElements;
             other.pElements = nullptr;
         }
 
+        /**
+         * @brief Destructor. Releases allocated memory.
+         */
         ~StaticArray()
         {
             delete[] this->pElements;
             this->pElements = NULL;
         }
 
+        /**
+         * @brief Gets value at specific index.
+         * @param index Index of element to get.
+         * @throws std::out_of_range if index is out of array bounds.
+         * @return Reference to element of template type T.
+         */
         T& get(const size_t index)
         {
             if (index >= size)
@@ -68,6 +104,12 @@ namespace dsa::structures
             return this->pElements[index];
         }
 
+        /**
+         * @brief Gets const value at specific index.
+         * @param index Index of element to get.
+         * @throws std::out_of_range if index is out of array bounds.
+         * @return Const reference to element of template type T.
+         */
         const T& get(const size_t index) const
         {
             if (index >= size)
@@ -76,31 +118,60 @@ namespace dsa::structures
             return this->pElements[index];
         }
 
+        /**
+         * @brief Gets size of array.
+         * @return Number of elements.
+         */
         constexpr int getSize()
         {
             return size;
         }
 
+        /**
+         * @brief Gets element at specific index using operator[].
+         * @param index Index of element.
+         * @return Reference to element of template type T.
+         */
         T& operator[](const size_t index)
         {
             return this->get(index);
         }
 
+        /**
+         * @brief Gets const element at specific index using operator[].
+         * @param index Index of element.
+         * @return Const reference to element of template type T.
+         */
         const T& operator[](const size_t index) const
         {
             return this->get(index);
         }
 
+        /**
+         * @brief Gets first element.
+         * @return Reference to first element of template type T.
+         */
         T& first()
         {
             return this->get(0);
         }
 
+        /**
+         * @brief Gets last element.
+         * @return Reference to last element of template type T.
+         */
         T& last()
         {
             return this->get(size - 1);
         }
 
+        /**
+         * @brief Sets element value at specific index.
+         *
+         * @param index Index of element to set.
+         * @param value Value to set at specific index.
+         * @throws std::out_of_range if index is out of array bounds.
+         */
         void set(const size_t index, T value)
         {
             if (index >= size)
@@ -109,6 +180,12 @@ namespace dsa::structures
             this->pElements[index] = value;
         }
 
+        /**
+         * @brief Replace array values by values from std::initializer_list using operator=.
+         * @param list Initializer list of elements to set.
+         * @throws std::runtime_error if list size does not match array size from template parameter.
+         * @return Reference to this StaticArray.
+         */
         StaticArray<T, size>& operator=(const std::initializer_list<T>& list)
         {
             if (size != list.size())
@@ -127,7 +204,11 @@ namespace dsa::structures
             return *this;
         }
 
-        // copy assignment operator
+        /**
+         * @brief Copy assignment operator.
+         * @param other StaticArray to copy from.
+         * @return Reference to this StaticArray.
+         */
         StaticArray<T, size>& operator=(const StaticArray<T, size>& other)
         {
             if (this == other)
@@ -145,7 +226,11 @@ namespace dsa::structures
             return *this;
         }
 
-        // move assignment operator
+        /**
+         * @brief Move assignment operator.
+         * @param other StaticArray to copy from.
+         * @return Reference to this StaticArray.
+         */
         StaticArray<T, size>& operator=(StaticArray<T, size>&& other) noexcept
         {
             if (this == &other)
@@ -159,6 +244,12 @@ namespace dsa::structures
             return *this;
         }
 
+        /**
+         * @brief Outputs content of array to stream.
+         * @param os Output stream.
+         * @param array Array to output.
+         * @return Reference to output stream.
+         */
         friend std::ostream& operator<<(std::ostream& os, const StaticArray<T, size>& array)
         {
             for (size_t i = 0; i < size; i++)
@@ -169,11 +260,19 @@ namespace dsa::structures
             return os;
         }
 
+        /**
+         * @brief Gets pointer to first element (for range-based for loops).
+         * @return Pointer to first element.
+         */
         T* begin()
         {
             return this->pElements;
         }
 
+        /**
+         * @brief Gets pointer to last element (for range-based for loops).
+         * @return Pointer to last element.
+         */
         T* end()
         {
             return this->pElements + size;
